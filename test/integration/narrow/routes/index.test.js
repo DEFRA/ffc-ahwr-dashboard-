f8d6751bc68a8d7d5b8a79608cdd5ba2fa8d0507
Alex White <alex.a.white@capgemini.com>
2024-01-08 17:06:06 +0000

    Tests

--- test/integration/narrow/routes/index.test.js
+++ test/integration/narrow/routes/index.test.js
@@ -1,6 +1,3 @@
-const cheerio = require('cheerio')
-const expectPhaseBanner = require('../../../utils/phase-banner-expect')
-
 describe('Dashboard home page test', () => {
   test('GET / route returns 200', async () => {
     const options = {
@@ -10,8 +7,6 @@ describe('Dashboard home page test', () => {
 
     const res = await global.__SERVER__.inject(options)
 
-    expect(res.statusCode).toBe(200)
-    const $ = cheerio.load(res.payload)
-    expectPhaseBanner.ok($)
+    expect(res.statusCode).toBe(302)
   })
 })
