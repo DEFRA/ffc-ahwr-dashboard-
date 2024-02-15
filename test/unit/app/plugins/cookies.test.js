ec608b22039393b2200820488ed979387f1f1177
govTechSatish <satish.chatap@capgemini.com>
2024-02-15 21:36:18 +0000

    more test

---
+++ test/unit/app/plugins/cookies.test.js
@@ -0,0 +1,85 @@
+const Hapi = require('@hapi/hapi')
+const HttpStatus = require('http-status-codes')
+const { plugin } = require('../../../../app/plugins/cookies')
+jest.mock('../../../../app/config', () => ({
+  ...jest.requireActual('../../../../app/config'),
+  cookie: {
+    cookieNameCookiePolicy: 'testCookiePolicyName',
+    cookieNameAuth: 'ffc_ahwr_auth',
+    cookieNameSession: 'ffc_ahwr_session',
+    isSameSite: 'Lax',
+    isSecure: true,
+    password: 'blah blah blah',
+    ttl: 1000 * 3600 * 24 * 3 // 3 days
+  },
+  cookiePolicy: {
+    clearInvalid: false,
+    encoding: 'base64json',
+    isSameSite: 'Lax',
+    isSecure: true,
+    password: 'blah blah blah',
+    ttl: 1000 * 3600 * 24 * 3,
+    path: '/'
+  }
+}))
+jest.mock('../../../../app/cookies', () => ({
+  getCurrentPolicy: jest.fn()
+}))
+
+describe('Cookie plugin', () => {
+  let server
+
+  beforeEach(async () => {
+    server = Hapi.server()
+    await server.register({ plugin })
+  })
+
+  test('should configure server state with cookie policy', () => {
+    // Assuming cookieNameCookiePolicy is the name of the cookie to be set
+    const cookieOptions = server.states.settings.cookies.testCookiePolicyName
+    expect(cookieOptions).toBeDefined()
+    expect(cookieOptions).toMatchObject({
+      // Your expected cookiePolicy object
+    })
+  })
+
+  describe('onPreResponse', () => {
+    let request
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+      server.route([{
+        method: 'GET',
+        path: '/',
+        handler: (req, h) => {
+          return h.view('index', { /* context */ })
+        }
+      }, {
+        method: 'GET',
+        path: '/blah',
+        handler: (req, h) => {
+          return 'Hello, world!'
+        }
+      }])
+    })
+
+    test('should not modify response when status code is NOT_FOUND', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/abc'
+      })
+      expect(response.statusCode).toEqual(HttpStatus.StatusCodes.NOT_FOUND)
+    })
+
+    test('should apply cookie policy for non-error, view responses', async () => {
+      const getCurrentPolicy = require('../../../../app/cookies').getCurrentPolicy
+      getCurrentPolicy.mockReturnValue({ /* Your mock policy data */ })
+      request = {
+        method: 'get',
+        url: '/blah'
+      }
+      const response = await server.inject(request)
+      expect(response.statusCode).toEqual(HttpStatus.StatusCodes.OK)
+    })
+  })
+})
