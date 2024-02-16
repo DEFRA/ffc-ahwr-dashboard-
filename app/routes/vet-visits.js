fd37c92ee51cea2d67331c0428727b2a2883c20f
Afshin-Karamifar-Capgemini <afshin.karamifar@capgemini.com>
2024-02-16 14:57:28 +0000

    Multiple business link has been added to the dashboard

--- app/routes/vet-visits.js
+++ app/routes/vet-visits.js
@@ -1,4 +1,5 @@
-const { getEndemicsClaim } = require('../session')
+const auth = require('../auth')
+const session = require('../session')
 const { vetVisits, claimServiceUri } = require('../config/routes')
 const {
   getLatestApplicationsBySbi
@@ -7,24 +8,26 @@ const {
 const pageUrl = `/${vetVisits}`
 const claimServiceRedirectUri = `${claimServiceUri}/endemics?from=dashboard`
 
-module.exports =
-  {
-    method: 'GET',
-    path: pageUrl,
-    options: {
-      handler: async (request, h) => {
-        const { organisation } = getEndemicsClaim(request)
-        const application = (
-          await getLatestApplicationsBySbi(organisation.sbi)
-        ).find((app) => {
-          return app.type === 'EE'
-        })
+module.exports = {
+  method: 'GET',
+  path: pageUrl,
+  options: {
+    handler: async (request, h) => {
+      const { attachedToMultipleBusinesses } = session.getCustomer(request)
+      const { organisation } = session.getEndemicsClaim(request)
+      const application = (
+        await getLatestApplicationsBySbi(organisation.sbi)
+      ).find((app) => {
+        return app.type === 'EE'
+      })
 
-        return h.view(vetVisits, {
-          claimServiceRedirectUri: `${claimServiceRedirectUri}&sbi=${organisation.sbi}`,
-          ...organisation,
-          ...(application?.reference && { reference: application?.reference })
-        })
-      }
+      return h.view(vetVisits, {
+        hasMultipleBusinesses: attachedToMultipleBusinesses,
+        claimServiceRedirectUri: `${claimServiceRedirectUri}&sbi=${organisation.sbi}`,
+        ...organisation,
+        ...(application?.reference && { reference: application?.reference }),
+        ...(attachedToMultipleBusinesses && { hostname: auth.requestAuthorizationCodeUrl(session, request) })
+      })
     }
   }
+}
