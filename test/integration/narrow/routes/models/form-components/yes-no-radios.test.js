5770a1228cfc35a622ec5662cae2903113fa906c
govTechSatish <satish.chatap@capgemini.com>
2024-02-16 11:51:20 +0000

    few more test

---
+++ test/integration/narrow/routes/models/form-components/yes-no-radios.test.js
@@ -0,0 +1,70 @@
+const { getYesNoRadios } = require('../../../../../../app/routes/models/form-component/yes-no-radios')
+
+describe('getYesNoRadios', () => {
+  const legendText = 'Do you agree?'
+  const id = 'agree'
+  const options = { isPageHeading: false, legendClasses: 'custom-class', inline: true, hintText: 'Hint text' }
+
+  test('should return correct structure with default options', () => {
+    const result = getYesNoRadios(legendText, id, 'yes')
+
+    expect(result).toEqual({
+      radios: {
+        classes: undefined, // Not inline by default
+        idPrefix: id,
+        name: id,
+        fieldset: {
+          legend: {
+            text: legendText,
+            isPageHeading: true, // Default value
+            classes: 'govuk-fieldset__legend--l', // Default value
+          },
+        },
+        hint: {
+          text: '', // Default empty string
+        },
+        items: [
+          { value: 'yes', text: 'Yes', checked: true },
+          { value: 'no', text: 'No', checked: false },
+        ],
+      },
+    })
+  })
+
+  test('should handle custom options and error text', () => {
+    const errorText = 'Error message'
+    const result = getYesNoRadios(legendText, id, 'no', errorText, options)
+
+    expect(result).toEqual({
+      radios: {
+        classes: 'govuk-radios--inline', // Inline due to options
+        idPrefix: id,
+        name: id,
+        fieldset: {
+          legend: {
+            text: legendText,
+            isPageHeading: options.isPageHeading,
+            classes: options.legendClasses,
+          },
+        },
+        hint: {
+          text: options.hintText,
+        },
+        items: [
+          { value: 'yes', text: 'Yes', checked: false },
+          { value: 'no', text: 'No', checked: true },
+        ],
+        errorMessage: {
+          text: errorText,
+        },
+      },
+    })
+  })
+
+  test('should correctly handle no previous answer', () => {
+    const result = getYesNoRadios(legendText, id, undefined, undefined, options)
+
+    expect(result.radios.items[0].checked).toBeFalsy()
+    expect(result.radios.items[1].checked).toBeFalsy()
+  })
+})
