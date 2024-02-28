1792e81fb783fecb741e297384fea92de6113591
Afshin <147500504+Afshin-Karamifar-Capgemini@users.noreply.github.com>
2024-02-28 17:06:06 +0000

    Ticket-305680: manage claims screen done (#38)
    * ticket-305680: manage claims screen done
    
    * Trigger pipeline

--- app/routes/vet-visits.js
+++ app/routes/vet-visits.js
@@ -1,9 +1,10 @@
 const auth = require('../auth')
 const session = require('../session')
 const { vetVisits, claimServiceUri } = require('../config/routes')
-const {
-  getLatestApplicationsBySbi
-} = require('../api-requests/application-api')
+const { getLatestApplicationsBySbi } = require('../api-requests/application-api')
+const { getClaimsByApplicationReference, isWithInLastTenMonths } = require('../api-requests/claim-api')
+const { claimType } = require('../constants/claim')
+const { statusId, statusClass } = require('../constants/status')
 
 const pageUrl = `/${vetVisits}`
 const claimServiceRedirectUri = `${claimServiceUri}/endemics?from=dashboard`
@@ -13,19 +14,32 @@ module.exports = {
   path: pageUrl,
   options: {
     handler: async (request, h) => {
-      const { attachedToMultipleBusinesses } = session.getCustomer(request)
+      const MAXIMUM_CLAIMS_TO_DISPLAY = 6
+      const VETVISIT_APPLICATION_TYPE = 'VV'
       const { organisation } = session.getEndemicsClaim(request)
-      const application = (
-        await getLatestApplicationsBySbi(organisation.sbi)
-      ).find((app) => {
-        return app.type === 'EE'
-      })
+      const { attachedToMultipleBusinesses } = session.getCustomer(request)
+      const applications = await getLatestApplicationsBySbi(organisation.sbi)
+      const vetVisitApplications = applications?.filter((application) => application.type === 'VV')
+      const latestEndemicsApplication = applications?.find((application) => application.type === 'EE')
+
+      let claims = await getClaimsByApplicationReference(latestEndemicsApplication.reference) || []
+      const vetVisitApplicationsWithInLastTenMonths = vetVisitApplications.filter((application) => isWithInLastTenMonths(application?.createdAt))
+
+      const capitalize = (value) => value?.charAt(0).toUpperCase() + value?.slice(1)
+      const sortByCreatedAt = (claims) => claims?.sort((a, b) => new Date(a.createdAt) > new Date(b.createdAt) ? a : b)
+      const typeOfReviewTitle = (typeOfReview) => [claimType.review, VETVISIT_APPLICATION_TYPE].includes(typeOfReview) ? 'Health and welfare review' : 'Endemic disease follow-ups'
+      const statusTag = (claim) => `<strong class=${statusClass[statusId[claim.statusId]]?.styleClass || 'govuk-tag--grey'}>${statusId[claim.statusId]}</strong>`
+      const description = (claim) => `${claim.reference} - ${claim.data?.typeOfLivestock ? capitalize(claim.data?.typeOfLivestock) + ' - ' : ''} ${typeOfReviewTitle(claim.type)}`
+
+      claims = [...(claims && sortByCreatedAt(claims)), ...(vetVisitApplicationsWithInLastTenMonths && sortByCreatedAt(vetVisitApplicationsWithInLastTenMonths))]
+      claims = claims.slice(0, MAXIMUM_CLAIMS_TO_DISPLAY).map(claim => ([{ text: description(claim) }, { html: statusTag(claim) }]))
 
       return h.view(vetVisits, {
+        claims,
         hasMultipleBusinesses: attachedToMultipleBusinesses,
         claimServiceRedirectUri: `${claimServiceRedirectUri}&sbi=${organisation.sbi}`,
         ...organisation,
-        ...(application?.reference && { reference: application?.reference }),
+        ...(latestEndemicsApplication?.reference && { reference: latestEndemicsApplication?.reference }),
         ...(attachedToMultipleBusinesses && { hostname: auth.requestAuthorizationCodeUrl(session, request) })
       })
     }
