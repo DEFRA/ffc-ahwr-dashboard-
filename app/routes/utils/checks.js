9820a30054ec2e0aa76c4b9896f0e02c4360f74f
takano <tosin.akano@capgemini.com>
2024-04-09 08:51:26 +0100

    refactor for visitDate

--- app/routes/utils/checks.js
+++ app/routes/utils/checks.js
@@ -1,9 +1,9 @@
 const { statusIdToFrontendStatusMapping } = require('../../constants/status')
 
 const isWithInLastTenMonths = require('../../api-requests/claim-api').isWithInLastTenMonths
-const checkStatusTenMonths = (claimData) => claimData?.some((claim) => isWithInLastTenMonths(claim?.createdAt) &&
-        (statusIdToFrontendStatusMapping[claim?.statusId] === 'PAID' ||
-        statusIdToFrontendStatusMapping[claim?.statusId] === 'READY TO PAY' ||
-        statusIdToFrontendStatusMapping[claim?.statusId] === 'CLAIMED'))
-
+const checkStatusTenMonths = (claimData) => {
+    return claimData?.some((claim) =>isWithInLastTenMonths(claim?.data?.visitDate) &&
+        (statusIdToFrontendStatusMapping[claim?.statusId] === 'PAID' || statusIdToFrontendStatusMapping[claim?.statusId] === 'READY TO PAY'))
+    
+}
 module.exports = { checkStatusTenMonths }
