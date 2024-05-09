aa30047f142118ef1101b86484fbe436b6ba3d75
Alex White <alex.a.white@capgemini.com>
2024-05-09 09:27:40 +0100

    Fix tests to cover all scenarios

--- app/routes/utils/checks.js
+++ app/routes/utils/checks.js
@@ -2,8 +2,11 @@ const { status } = require('../../constants/status')
 
 const isWithInLastTenMonths = require('../../api-requests/claim-api').isWithInLastTenMonths
 
-const checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths = (claimData) => claimData?.some((claim) => ((isWithInLastTenMonths(claim?.data?.visitDate) || isWithInLastTenMonths(claim?.data?.dateOfVisit)) &&
-            (claim?.statusId === status.PAID || claim?.statusId === status.READY_TO_PAY)) &&
-            (claim?.type === 'VV' || claim?.type === 'R'))
+const checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths = (claimData) => claimData?.some((claim) => 
+(
+    (isWithInLastTenMonths(claim?.data?.visitDate) || isWithInLastTenMonths(claim?.data?.dateOfVisit)) && 
+    (claim?.statusId === status.PAID || claim?.statusId === status.READY_TO_PAY) && 
+    (claim?.type === 'VV' || claim?.type === 'R')
+))
 
 module.exports = { checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths }
--- test/integration/narrow/routes/utils/checks.test.js
+++ test/integration/narrow/routes/utils/checks.test.js
@@ -7,43 +7,41 @@ describe('checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths', () => {
     jest.mock('../../../../../app/api-requests/claim-api')
   })
 
-  test('should return true if claimData contains a claim within the last ten months with a valid status', () => {
+  test.each([
+    { data: { visitDate: new Date('2024-01-01') }, statusId: 8, type: 'VV' },
+    { data: { visitDate: new Date('2024-02-01') }, statusId: 8, type: 'R' },
+    { data: { visitDate: new Date('2024-03-01') }, statusId: 9, type: 'VV' },
+    { data: { visitDate: new Date('2024-03-01') }, statusId: 9, type: 'R' }
+  ])('should return true if claimData contains a claim within the last ten months with a valid status', (claimData) => {
     mockClaimApi.isWithInLastTenMonths.mockReturnValue(true)
-    const claimData = [
-      { data: { visitDate: new Date('2024-01-01') }, statusId: 8, type: 'VV' },
-      { data: { visitDate: new Date('2024-02-01') }, statusId: 8, type: 'R' },
-      { data: { visitDate: new Date('2024-03-01') }, statusId: 8, type: 'VV' }
-    ]
 
-    const result = checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths(claimData)
+    const result = checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths([claimData])
 
     expect(result).toBeTruthy()
   })
 
-  test('should return false if claimData does not contain a claim within the last ten months', () => {
+  test.each([
+    { data: { visitDate: new Date('2021-01-01') }, statusId: 8, type: 'VV' },
+    { data: { visitDate: new Date('2021-01-01') }, statusId: 8, type: 'VV' },
+    { data: { visitDate: new Date('2021-02-01') }, statusId: 9, type: 'R' },
+    { data: { visitDate: new Date('2021-02-01') }, statusId: 9, type: 'R' }
+  ])('should return false if claimData does not contain a claim within the last ten months', (claimData) => {
     mockClaimApi.isWithInLastTenMonths.mockReturnValue(false)
-    const claimData = [
-      { data: { visitDate: new Date('2021-01-01') }, statusId: 8, type: 'VV' },
-      { data: { visitDate: new Date('2021-01-01') }, statusId: 8, type: 'VV' },
-      { data: { visitDate: new Date('2021-02-01') }, statusId: 9, type: 'R' },
-      { data: { visitDate: new Date('2021-02-01') }, statusId: 9, type: 'R' }
-    ]
 
-    const result = checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths(claimData)
+    const result = checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths([claimData])
 
     expect(result).toBeFalsy()
   })
 
-  test('should return false if claimData contains a claim within the last ten months but with an invalid status', () => {
+  test.each([
+    { data: { visitDate: new Date('2022-01-01') }, statusId: 1, type: 'VV' },
+    { data: { visitDate: new Date('2022-02-01') }, statusId: 2, type: 'R' },
+    { data: { visitDate: new Date('2022-02-01') }, statusId: 5, type: 'VV' },
+    { data: { visitDate: new Date('2022-02-01') }, statusId: 3, type: 'R' }
+  ])('should return false if claimData contains a claim within the last ten months but with an invalid status', (claimData) => {
     mockClaimApi.isWithInLastTenMonths.mockReturnValue(true)
-    const claimData = [
-      { data: { visitDate: new Date('2022-01-01') }, statusId: 1, type: 'VV' },
-      { data: { visitDate: new Date('2022-02-01') }, statusId: 2, type: 'R' },
-      { data: { visitDate: new Date('2022-02-01') }, statusId: 5, type: 'VV' },
-      { data: { visitDate: new Date('2022-02-01') }, statusId: 3, type: 'R' }
-    ]
 
-    const result = checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths(claimData)
+    const result = checkReviewIsPaidOrReadyToPayAndWithinLastTenMonths([claimData])
 
     expect(result).toBeFalsy()
   })
