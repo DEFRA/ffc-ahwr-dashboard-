22f9a23961724d81dd802172785d5dc523a73791
Matthew O'Donoghue <mattyod@gmail.com>
2024-12-16 16:59:58 +0000

    feat(AHWR-251): add sortable table for claims (#133)

--- app/routes/utils/checks.js
+++
@@ -1,9 +0,0 @@
-const { status } = require('../../constants/status')
-
-const checkReviewIsPaidOrReadyToPay = (claimData) => claimData?.some((claim) =>
-  (
-    (claim?.statusId === status.PAID || claim?.statusId === status.READY_TO_PAY) &&
-    (claim?.type === 'VV' || claim?.type === 'R')
-  ))
-
-module.exports = { checkReviewIsPaidOrReadyToPay }
--- test/integration/narrow/routes/vet-visits.test.js
+++ test/integration/narrow/routes/vet-visits.test.js
@@ -11,14 +11,10 @@ const cheerio = require('cheerio')
 jest.mock('../../../../app/session')
 jest.mock('../../../../app/api-requests/application-api')
 jest.mock('../../../../app/api-requests/claim-api')
-jest.mock('../../../../app/routes/utils/checks')
 const HttpStatus = require('http-status-codes')
-const { claimType } = require('../../../../app/constants/claim')
-const checks = require('../../../../app/routes/utils/checks')
 
 describe('Claim vet-visits', () => {
   beforeAll(async () => {
-    checks.checkReviewIsPaidOrReadyToPay.mockReturnValue(false)
     jest.mock('../../../../app/config', () => ({
       ...jest.requireActual('../../../../app/config'),
       endemics: {
@@ -27,89 +23,7 @@ describe('Claim vet-visits', () => {
     }))
   })
 
-  const organisation = { sbi: '112670111' }
-  const attachedToMultipleBusinesses = true
-
-  const applications = [
-    {
-      id: 'b13676a0-3a57-428e-a903-9dcf6eca104b',
-      reference: 'AHWR-B136-76A0',
-      type: 'VV'
-    },
-    {
-      id: 'b13676a0-3a57-428e-a903-9dcf6eca104b',
-      reference: 'AHWR-B136-76A0',
-      type: 'EE'
-    }
-  ]
-
   const url = `/${vetVisits}`
-  const claims = [
-    {
-      id: 'a94e2cce-b774-484f-95c0-94e93c82f311',
-      reference: 'AHWR-A94E-2CCE',
-      applicationReference: 'AHWR-B136-76A0',
-      data: {
-        vetsName: 'Afshin',
-        dateOfVisit: '2024-02-28T00:00:00.000Z',
-        dateOfTesting: '2024-02-28T00:00:00.000Z',
-        laboratoryURN: 'URN4567',
-        vetRCVSNumber: '1234567',
-        speciesNumbers: 'yes',
-        typeOfLivestock: 'sheep',
-        numberAnimalsTested: '21'
-      },
-      statusId: 8,
-      type: 'R',
-      createdAt: '2024-02-26T14:14:43.632Z',
-      updatedAt: '2024-02-28T15:09:03.185Z',
-      createdBy: 'admin',
-      updatedBy: null,
-      status: { status: 'ON HOLD' }
-    },
-    {
-      id: 'a94e2cce-b774-484f-95c0-94e93c82f311',
-      reference: 'AHWR-A94E-2CCE',
-      applicationReference: 'AHWR-B136-76A0',
-      data: {
-        vetsName: 'Afshin',
-        dateOfVisit: '2024-02-28T00:00:00.000Z',
-        dateOfTesting: '2024-02-28T00:00:00.000Z',
-        laboratoryURN: 'URN4567',
-        vetRCVSNumber: '1234567',
-        speciesNumbers: 'yes',
-        numberAnimalsTested: '21'
-      },
-      statusId: 18,
-      type: 'E',
-      createdAt: '2024-02-28T14:14:43.632Z',
-      updatedAt: '2024-02-28T15:09:03.185Z',
-      createdBy: 'admin',
-      updatedBy: null,
-      status: { status: 'ON HOLD' }
-    },
-    {
-      id: 'a94e2cce-b774-484f-95c0-94e93c82f311',
-      reference: 'AHWR-A94E-2CCE',
-      applicationReference: 'AHWR-B136-76A0',
-      data: {
-        vetsName: 'Afshin',
-        dateOfVisit: '2024-02-28T00:00:00.000Z',
-        dateOfTesting: '2024-02-28T00:00:00.000Z',
-        laboratoryURN: 'URN4567',
-        vetRCVSNumber: '1234567',
-        speciesNumbers: 'yes',
-        numberAnimalsTested: '21'
-      },
-      statusId: 18,
-      type: 'E',
-      createdAt: '2024-02-27T14:14:43.632Z',
-      updatedAt: '2024-02-28T15:09:03.185Z',
-      createdBy: 'admin',
-      updatedBy: null,
-      status: { status: 'ON HOLD' }
-    }
-  ]
 
   test('GET /vet-visits route returns 200', async () => {
     const applications = [
@@ -258,8 +172,10 @@ describe('Claim vet-visits', () => {
 
     expect($('#SBI').text()).toEqual(SBIText)
     expect($('#MBILink').text()).toEqual('Claim for a different business')
+    expect($('tbody tr').length).toBe(3)
     expect(response.statusCode).toBe(HttpStatus.StatusCodes.OK)
   })
+
   test('GET /vet-visits route without any claim returns 200', async () => {
     const applications = [
       {
@@ -306,18 +222,16 @@ describe('Claim vet-visits', () => {
     })
 
     getLatestApplicationsBySbi.mockReturnValueOnce(applications)
-
-    getClaimsByApplicationReference.mockReturnValueOnce()
+    getCustomer.mockReturnValueOnce({ attachedToMultipleBusinesses: true })
+    getClaimsByApplicationReference.mockReturnValueOnce([])
 
     const response = await global.__SERVER__.inject(options)
     const $ = cheerio.load(response.payload)
-    const SBIText = 'Single Business Identifier (SBI): 112670111'
 
-    expect($('#SBI').text()).toEqual(SBIText)
-    expect($('#MBILink').text()).toEqual('Claim for a different business')
-    const button = $('.govuk-main-wrapper .govuk-button')
+    const button = $('.govuk-grid-row > .govuk-button')
     expect(button.text()).toMatch('Start a new claim')
     expect(response.statusCode).toBe(HttpStatus.StatusCodes.OK)
+    expect($('tbody tr').length).toBe(0)
   })
 
   test('GET /vet-visits route returns 302', async () => {
@@ -362,78 +276,6 @@ describe('Claim vet-visits', () => {
 
     expect(getClaimsByApplicationReference)
       .toHaveBeenCalledWith(latestEndemicsApplication.reference, expect.any(Object))
-  })
-  test('typeOfReviewTitle returns review', () => {
-    const typeOfReviewTitle = (typeOfReview) => [claimType.review, 'VV'].includes(typeOfReview) ? 'review' : 'follow-up'
-    const typeOfReview = claimType.review
-    const result = typeOfReviewTitle(typeOfReview)
-    expect(result).toBe('review')
-  })
-
-  test('typeOfReviewTitle returns follow-up', () => {
-    const typeOfReviewTitle = (typeOfReview) => [claimType.review, 'VV'].includes(typeOfReview) ? 'review' : 'follow-up'
-    const typeOfReview = 'claimType.endemics'
-    const result = typeOfReviewTitle(typeOfReview)
-    expect(result).toBe('follow-up')
-  })
-  test('statusTag returns status tag', () => {
-    claims.statusId = 8
-
-    const statusIdToFrontendStatusMapping = {
-      8: 'PAID'
-    }
-    const statusTag = (claims) => `<strong class="govuk-tag ${'govuk-tag-- app-task-list__tag'}">${statusIdToFrontendStatusMapping[claims.statusId]}</strong>`
-    expect(statusTag(claims)).toMatch('<strong class="govuk-tag govuk-tag-- app-task-list__tag">PAID</strong>')
-  })
-  describe('description', () => {
-    const description = (claim) => {
-      const { reference, data, type } = claim
-      const livestock = data.typeOfLivestock || data.whichReview
-      const review = type === 'R' ? 'review' : 'follow-up'
-      return `${reference} - ${livestock} ${review} ${type}`
-    }
-
-    test('should return the correct description when typeOfLivestock is present', () => {
-      const claim = {
-        reference: 'AHWR-A94E-2CCE',
-        data: {
-          typeOfLivestock: 'sheep'
-        },
-        type: 'R'
-      }
-
-      const result = description(claim)
-
-      expect(result).toMatch('AHWR-A94E-2CCE - sheep review')
-    })
-
-    test('should return the correct description when typeOfLivestock is not present', () => {
-      const claim = {
-        reference: 'AHWR-A94E-2CCE',
-        data: {
-          whichReview: 'cattle'
-        },
-        type: 'E'
-      }
-
-      const result = description(claim)
-
-      expect(result).toMatch('AHWR-A94E-2CCE - cattle follow-up')
-    })
-  })
-  describe('helper methods - vet visits ', () => {
-    getEndemicsClaim.mockReturnValueOnce({ organisation })
-    const sortByCreatedAt = (claims) => {
-      return claims.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-    }
-
-    getCustomer.mockReturnValueOnce({ attachedToMultipleBusinesses })
-    getLatestApplicationsBySbi.mockReturnValueOnce(applications)
-    getClaimsByApplicationReference.mockReturnValueOnce(claims)
-    test('sortByCreatedAt should return sorted claims', () => {
-      const sortedClaims = sortByCreatedAt(claims)
-      expect(sortedClaims).toEqual(claims.sort(claims?.createdAt))
-    })
   })
 
   test('shows notification banner when enabled', async () => {
