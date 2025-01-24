cc80cb98088754d75c6625c20cf5d5ce848b37a0
Matt O'Donoghue <mattyod@gmail.com>
2025-01-24 17:42:40 +0000

    use global mocks for block storage tests
    You can now also pass a file name to the mock and it will use that to
    create the returned buffer rather than a hard coded string.
    
    So `getBlob('filename')` will return a buffer of `filename.pdf`
    
    You can also assert on whether the static method was called or not.

--- test/unit/app/storage.test.js
+++ test/unit/app/storage.test.js
@@ -1,32 +1,11 @@
 const { BlobServiceClient } = require('@azure/storage-blob')
 const { getBlob, resetClient } = require('../../../app/storage')
-const { streamToBuffer } = require('../../../app/lib/streamToBuffer')
 const { storage } = require('../../../app/config')
 
-jest.mock('@azure/storage-blob')
-jest.mock('@azure/identity')
-jest.mock('../../../app/lib/streamToBuffer')
-
-const mockFromConnectionString = jest.fn(() => ({
-  getContainerClient: jest.fn(() => ({
-    getBlobClient: jest.fn(() => ({
-      download: jest.fn(() => ({
-        readableStreamBody: 'fakeStream'
-      }))
-    }))
-  }))
-}))
-BlobServiceClient.fromConnectionString = mockFromConnectionString
-
 describe('Blob Storage Service', () => {
-  beforeEach(() => {
-    streamToBuffer.mockResolvedValue('fakeBufferContent')
-  })
-
   afterEach(() => {
     resetClient()
     jest.resetAllMocks()
-    jest.resetModules()
   })
 
   it('should initialize client with connection string and return buffer content', async () => {
@@ -36,20 +15,14 @@ describe('Blob Storage Service', () => {
     const blobContent = await getBlob('fakeFile.txt')
 
     expect(BlobServiceClient.fromConnectionString).toHaveBeenCalledWith('fakeConnectionString')
-    expect(blobContent).toEqual('fakeBufferContent')
+    expect(Buffer.from(blobContent).toString()).toEqual('fakeFile.txt')
   })
 
   it('should initialize client with managed identity', async () => {
     storage.useConnectionString = false
-    storage.applicationDocumentsContainer = 'fakeContainer'
-    storage.storageAccount = 'fakeAccount'
-
-    process.env.AZURE_CLIENT_ID = 'fakeClientId'
-
     const blobContent = await getBlob('fakeFile.txt')
 
     expect(BlobServiceClient.fromConnectionString).toHaveBeenCalledTimes(0)
-
-    expect(blobContent).toEqual('fakeBufferContent')
+    expect(Buffer.from(blobContent).toString()).toEqual('fakeFile.txt')
   })
 })
