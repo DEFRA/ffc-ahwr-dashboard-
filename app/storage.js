b5102f5f6d833fe2d28ce00782d474114b2d99c3
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-01-24 09:47:17 +0000

    feat(AHWR-470): Migrate to new jenkins pipeline and workload identity

--- app/storage.js
+++ app/storage.js
@@ -3,28 +3,32 @@ const { DefaultAzureCredential } = require('@azure/identity')
 const { storage } = require('./config')
 const { streamToBuffer } = require('./lib/streamToBuffer')
 
-const getBlob = async (filename) => {
-  const {
-    connectionString,
-    useConnectionString,
-    applicationDocumentsContainer,
-    storageAccount
-  } = storage
+const {
+  connectionString,
+  useConnectionString,
+  applicationDocumentsContainer,
+  storageAccount
+} = storage
 
-  let blobServiceClient
-
-  if (useConnectionString === true) {
-    blobServiceClient = BlobServiceClient.fromConnectionString(connectionString)
-  } else {
-    const uri = `https://${storageAccount}.blob.core.windows.net`
-    blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential())
+let blobServiceClient
+function initialiseClient () {
+  if (!blobServiceClient) {
+    if (useConnectionString === true) {
+      blobServiceClient = BlobServiceClient.fromConnectionString(connectionString)
+    } else {
+      const uri = `https://${storageAccount}.blob.core.windows.net`
+      blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential({ managedIdentityClientId: process.env.AZURE_CLIENT_ID }))
+    }
   }
+}
+
+const getBlob = async (filename) => {
+  initialiseClient()
 
   const container = blobServiceClient.getContainerClient(applicationDocumentsContainer)
   const blobClient = container.getBlobClient(filename)
   const downloadResponse = await blobClient.download()
-  const downloaded = await streamToBuffer(downloadResponse.readableStreamBody)
-  return downloaded
+  return await streamToBuffer(downloadResponse.readableStreamBody)
 }
 
 module.exports = {
