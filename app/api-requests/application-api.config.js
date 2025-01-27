4b871a93590bdcecb0932a83def30e39eac5307b
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2025-01-27 20:57:48 +0000

    chore: fix security hotspot

--- app/api-requests/application-api.config.js
+++ app/api-requests/application-api.config.js
@@ -1,7 +1,7 @@
 import joi from 'joi'
 
 export const applicationApiConfigSchema = joi.object({
-  uri: joi.string().uri().default('http://host.docker.internal:3001/api')
+  uri: joi.string().uri().required()
 })
 
 const getApplicationApiConfig = () => {
