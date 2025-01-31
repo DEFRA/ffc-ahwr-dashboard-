b9b856a927c598ae8c3d4edf14df46f4226e10dc
RobCatton <41433657+RobCatton@users.noreply.github.com>
2025-01-31 13:45:55 +0000

    fix - Layering on all pipeline changes ontop of working version (#148)

--- app/storage.js
+++ app/storage.js
@@ -3,28 +3,35 @@ const { DefaultAzureCredential } = require('@azure/identity')
 const { storage } = require('./config')
 const { streamToBuffer } = require('./lib/streamToBuffer')
 
-const getBlob = async (filename) => {
-  const {
-    connectionString,
-    useConnectionString,
-    applicationDocumentsContainer,
-    storageAccount
-  } = storage
+const {
+  applicationDocumentsContainer
+} = storage
 
-  let blobServiceClient
+let blobServiceClient
+function initialiseClient () {
+  if (!blobServiceClient) {
+    const {
+      connectionString,
+      useConnectionString,
+      storageAccount
+    } = storage
 
-  if (useConnectionString === true) {
-    blobServiceClient = BlobServiceClient.fromConnectionString(connectionString)
-  } else {
-    const uri = `https://${storageAccount}.blob.core.windows.net`
-    blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential())
+    if (useConnectionString === true) {
+      blobServiceClient = BlobServiceClient.fromConnectionString(connectionString)
+    } else {
+      const uri = `https://${storageAccount}.blob.core.windows.net`
+      blobServiceClient = new BlobServiceClient(uri, new DefaultAzureCredential({ managedIdentityClientId: process.env.AZURE_CLIENT_ID }))
+    }
   }
+}
+
+const getBlob = async (filename) => {
+  initialiseClient()
 
   const container = blobServiceClient.getContainerClient(applicationDocumentsContainer)
   const blobClient = container.getBlobClient(filename)
   const downloadResponse = await blobClient.download()
-  const downloaded = await streamToBuffer(downloadResponse.readableStreamBody)
-  return downloaded
+  return await streamToBuffer(downloadResponse.readableStreamBody)
 }
 
 module.exports = {
